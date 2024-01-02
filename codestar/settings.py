a61703a9f8d799f714c07bd640304b302ccdd4be
DanilleH22 <>
2024-01-02 21:46:45 +0000

    Add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,14 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-_#te3653y74b@%irnm&^tr$yr2+)7$-z9y#&72zdpaz3s6jtaf'
+# SECRET_KEY =
+# 'django-insecure-_#te3653y74b@%irnm&^tr$yr2+)7$-z9y#&72zdpaz3s6jtaf'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-danilleh22-blog-django-y8azn6n58w.us2.codeanyapp.com', '.herokuapp.com']
+    '8000-danilleh22-blog-django-y8azn6n58w.us2.codeanyapp.com',
+    '.herokuapp.com']
 
 
 # Application definition
@@ -91,21 +93,31 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.'
+        'password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.'
+        'password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.'
+        'password_validation.NumericPasswordValidator',
     },
 ]
 
